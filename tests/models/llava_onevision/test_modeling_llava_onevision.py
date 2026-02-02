2df36d1c04551bd0390b593ce80f09d1567362e8
Liu, Kaixuan <kaixuan.liu@intel.com>
2026-02-02 10:38:53 +0000

    fix LINT issue
    Signed-off-by: Liu, Kaixuan <kaixuan.liu@intel.com>

--- tests/models/llava_onevision/test_modeling_llava_onevision.py
+++ tests/models/llava_onevision/test_modeling_llava_onevision.py
@@ -212,7 +212,9 @@ class LlavaOnevisionForConditionalGenerationModelTest(ModelTesterMixin, Generati
     test_cpu_offload = False
     test_disk_offload_safetensors = False
     test_disk_offload_bin = False
-    test_torch_exportable = False  # uses data-dependent control flow with image_sizes, not compatible with torch.export
+    test_torch_exportable = (
+        False  # uses data-dependent control flow with image_sizes, not compatible with torch.export
+    )
     _is_composite = True
 
     def setUp(self):
